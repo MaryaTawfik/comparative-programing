40da65dafc5106ea6032e72b0421b670dc4e1d90
Marya Tawfik <maryaahmed200412@gmail.com>
2025-06-26 21:53:39 +0300

    Time: 0 ms (100.00%) | Memory: 20 MB (7.16%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,8 @@
+from collections import Counter
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        count=Counter(nums)
+        for i, j in count.items():
+            if j==1:
+                return i
+        
