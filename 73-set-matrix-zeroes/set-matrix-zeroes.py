6cfcf9d1c8b416bf182f9e23fe22bb0cffa9594c
Marya Tawfik <maryaahmed200412@gmail.com>
2026-03-02 21:42:49 +0200

    Time: 0 ms (100.00%) | Memory: 20.1 MB (60.72%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,29 +5,56 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        m, n = len(matrix), len(matrix[0])
-        first_row_zero = any(matrix[0][j] == 0 for j in range(n))
-        first_col_zero = any(matrix[i][0] == 0 for i in range(m))
-
-        # Use first row and column as markers
-        for i in range(1, m):
-            for j in range(1, n):
+        place=[]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    matrix[i][0] = 0
-                    matrix[0][j] = 0
+                    row=[i,j]
+                    place.append(row)
+                    # for k in range (len(matrix[0])):
+                    #     matrix[i][k] = 0
+                    # for l in range(len(matrix)):
+                    #     matrix[l][j] =0
+        for i in range(len(place)):
+            for k in range(len(matrix[0])):
+                matrix[place[i][0]][k]=0
+            for l in range(len(matrix)):
+                matrix[l][place[i][1]]=0
 
-        # Zero out cells based on markers
-        for i in range(1, m):
-            for j in range(1, n):
-                if matrix[i][0] == 0 or matrix[0][j] == 0:
-                    matrix[i][j] = 0
+        return matrix
 
-        # Handle first row
-        if first_row_zero:
-            for j in range(n):
-                matrix[0][j] = 0
 
-        # Handle first column
-        if first_col_zero:
-            for i in range(m):
-                matrix[i][0] = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # m, n = len(matrix), len(matrix[0])
+        # first_row_zero = any(matrix[0][j] == 0 for j in range(n))
+        # first_col_zero = any(matrix[i][0] == 0 for i in range(m))
+        # for i in range(1, m):
+        #     for j in range(1, n):
+        #         if matrix[i][j] == 0:
+        #             matrix[i][0] = 0
+        #             matrix[0][j] = 0
+        # for i in range(1, m):
+        #     for j in range(1, n):
+        #         if matrix[i][0] == 0 or matrix[0][j] == 0:
+        #             matrix[i][j] = 0
+        # if first_row_zero:
+        #     for j in range(n):
+        #         matrix[0][j] = 0
+        # if first_col_zero:
+        #     for i in range(m):
+        #         matrix[i][0] = 0
