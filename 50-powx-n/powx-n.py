369879fa983a673cdf96600db5cac3d7f1d9b15c
Marya Tawfik <maryaahmed200412@gmail.com>
2026-03-17 22:55:08 +0200

    Time: 3 ms (11.30%) | Memory: 19.4 MB (78.55%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,4 +1,23 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        power = x**n
-        return power
+        if n == 0:
+            return 1
+        
+        if n <0:
+           return 1/self.myPow(x,-n)
+        else:
+            if n%2==0:
+                half=self.myPow(x,n//2)
+                return half*half
+            else:
+                half=self.myPow(x,n//2)
+            return half*half*x
+        
+
+
+            
+
+        #     return 1/self.myPow(x,abs(n))
+        # return self.myPow(x,n-1) *x
+        # power = x**n
+        # return power
