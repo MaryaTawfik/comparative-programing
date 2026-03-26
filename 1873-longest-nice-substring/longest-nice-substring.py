13eb4d0ab44ae6db7925522b0e1da2aee4542d64
Marya Tawfik <maryaahmed200412@gmail.com>
2026-03-26 18:31:22 +0200

    Time: 1 ms (75.45%) | Memory: 19.1 MB (97.37%) - LeetSync

---
+++ 1873-longest-nice-substring/longest-nice-substring.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        seen=set(s)
+        if len(s)<2:
+            return ""
+        for i in range(len(s)):
+            if s[i].swapcase() not in seen:
+                left_side=self.longestNiceSubstring(s[:i])
+                right_side=self.longestNiceSubstring(s[i+1:])
+                return left_side if len(left_side)>=len(right_side) else right_side
+        return s
+
+
+        
