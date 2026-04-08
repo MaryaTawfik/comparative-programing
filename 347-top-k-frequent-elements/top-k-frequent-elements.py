5db83f8fa0800e8c3708cc2d210b6491341ae57b
Marya Tawfik <maryaahmed200412@gmail.com>
2026-04-08 17:09:11 +0200

    Time: 3 ms (88.38%) | Memory: 23 MB (48.73%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,13 +1,25 @@
 from collections import Counter
+from typing import List
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter=Counter(nums)
-        sorted_sec_ele= sorted(counter.items(), key=lambda x:x[1],reverse=True)
-        ans=[]
-        p=0
-        while len(ans)<k and p < len(sorted_sec_ele):
-            ans.append(sorted_sec_ele[p][0])
-            p+=1
-        return ans
+        bucket=Counter(nums)
+        bucket_=bucket.most_common(k)
+        return [num for num,freq in bucket_]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
 
         
