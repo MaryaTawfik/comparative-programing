26069fd4f8b54b5ca68ceb475639cd76c0b51c88
Marya Tawfik <maryaahmed200412@gmail.com>
2026-04-11 17:01:34 +0200

    Time: 3 ms (83.61%) | Memory: 19.1 MB (93.64%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -2,16 +2,13 @@ from typing import List
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        # Step 1: Record last occurrence of each character
         last = {ch: i for i, ch in enumerate(s)}
-        
+        # print(last)
         res = []
         start = end = 0
-        
-        # Step 2: Iterate through string
         for i, ch in enumerate(s):
             end = max(end, last[ch])
-            if i == end:  # Partition boundary
+            if i == end: 
                 res.append(end - start + 1)
                 start = i + 1
         
