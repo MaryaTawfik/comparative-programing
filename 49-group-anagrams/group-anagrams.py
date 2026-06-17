a1dc3ba03512de61c18d7fbca6e2b7365699ad60
Marya Tawfik <maryaahmed200412@gmail.com>
2026-06-17 10:05:11 +0300

    Time: 15 ms (43.79%) | Memory: 22.1 MB (66.49%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,34 +2,14 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        arr=[]
-        
+        my_dict=defaultdict(list)
+        mat=[]
         for i in strs:
-            s=list(i)
-            s.sort()
-            row=[]
-            row.append(s)
-            row.append(i)
-            arr.append(row)
-        arr.sort()
-        ans=[]
-        p1=0
-        while p1<len(arr):
-            row=[]
-            p2=p1
-            while p2< len(arr) and arr[p2][0]==arr[p1][0]:
-                row.append(arr[p2][1])
-                p2+=1
-           
-            
-            ans.append(row)
-            p1=p2
-            # print(row)
-            row=[]
-            
-        return ans
+            key="".join(sorted(i))
+            my_dict[key].append(i)
+        for val in my_dict.values():
+            mat.append(val)
+        return mat
 
 
-
-            
         
