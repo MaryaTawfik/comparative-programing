7e84abe075bb9bd3b8548f3198038bdb54d53458
Marya Tawfik <maryaahmed200412@gmail.com>
2026-06-17 09:35:33 +0300

    Time: 22 ms (11.35%) | Memory: 23.8 MB (27.48%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,12 +2,34 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        anagram_map = defaultdict(list)
+        arr=[]
         
-        for word in strs:
-            # Sort the word to form the key
-            key = "".join(sorted(word))
-            anagram_map[key].append(word)
+        for i in strs:
+            s=list(i)
+            s.sort()
+            row=[]
+            row.append(s)
+            row.append(i)
+            arr.append(row)
+        arr.sort()
+        ans=[]
+        p1=0
+        while p1<len(arr):
+            row=[]
+            p2=p1
+            while p2< len(arr) and arr[p2][0]==arr[p1][0]:
+                row.append(arr[p2][1])
+                p2+=1
+           
+            
+            ans.append(row)
+            p1=p2
+            # print(row)
+            row=[]
+            
+        return ans
+
+
+
+            
         
-        # Return grouped anagrams
-        return list(anagram_map.values())
