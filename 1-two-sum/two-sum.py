334647e4a23daf1ef7bc1aac60c1793b13d8f715
Marya Tawfik <maryaahmed200412@gmail.com>
2026-09-08 21:10:25 +0300

    Time: 11 ms (35.41%) | Memory: 21.5 MB (7.25%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,33 @@
+# from collections import defualtdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j]== target:
-                    return [i,j]
+        traker = defaultdict(list)
+        left , right = 0 , len(nums)-1
 
+        ans=[]
+
+        for i in range(len(nums)):
+            traker[nums[i]].append(i) 
+        print(traker)
+        nums.sort()
+        while left < right:
+
+            if nums[left] + nums[right] == target:
+                # return [traker[nums[left]] , traker[nums[right]]]
+                ans.append(traker[nums[left]][-1])
+                traker[nums[left]].pop()
+                ans.append(traker[nums[right]][-1])
+                traker[nums[right]].pop()
+                return ans
+                
+            elif nums[left] + nums[right] > target:
+                right -= 1
+            else:
+                left += 1
+
+
+
+        
 
 
 
