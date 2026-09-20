752f08abb29b64e4836b4c7fea5ed58e75c012b5
Marya Tawfik <maryaahmed200412@gmail.com>
2026-09-20 22:34:27 +0300

    Time: 194 ms (47.35%) | Memory: 33.5 MB (99.66%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,22 +1,44 @@
-from collections import deque
 class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        max_=deque()
-        left=0
-        right=0
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
         ans=[]
-        while right<len(nums):
-            while max_ and nums[max_[-1]]<nums[right]:
-                max_.pop()
-            max_.append(right)
-            if left > max_[0]:
-                max_.popleft()
-            if (right+1)>=k:
-                ans.append(nums[max_[0]])
-                left+=1
-            right+=1
+        # [4,3,2,1]
+        
+        l,r=0,0
+        que=collections.deque()
+        while r<len(nums):
+            while que and nums[r]>nums[que[-1]]:
+                que.pop()
+            que.append(r)
+            if l > que[0]:
+                que.popleft()
+            if r+1 >= k:
+                ans.append(nums[que[0]])
+                l+=1
+            r+=1
         return ans
+
+        # # [3,2,-1,-3,5,3,6,7]
+        # #    l  l    r
+        # val[1,-3]
+        # idx[2,3]
+        # ans=[3,2]
+        # left=0
+        # right=k-1
+        # # max_=nums[0]
+        # # for i in range(k):
+        # #     if max_ < nums[i]:
+        # #         max_=nums[i]
+        # # ans.append(max_)
+        # while left<=right and right<len(nums):
+        #     max_=float(-inf)
+        #     for i in range(left,right+1):
+        #         if max_ < nums[i]:
+        #             max_=nums[i]
+        #     ans.append(max_)
+        #     left+=1
+        #     right+=1
+        # return ans
+
+            
             
 
-
-            
