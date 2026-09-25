787f1279e158ef4f977a5c8c21f3f6a08e582c0a
Marya Tawfik <maryaahmed200412@gmail.com>
2026-09-25 19:30:51 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (80.78%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,38 +1,31 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if not matrix or not matrix[0]:
+        col=len(matrix[0])
+        row=len(matrix)
+        top , bot = 0 , row-1
+        pos=0
+        while top <= bot:
+            mid = (top + bot)//2
+
+            if matrix[mid][0] <= target and matrix[mid][-1] >= target:
+                pos=mid
+                break
+            if matrix[mid][-1] <= target:
+                top = mid+1
+            if matrix[mid][-1]>= target:
+                bot = mid-1
+        else:
             return False
-        #let first find the correct row then we will try to find the corrct col
-        #mid=(left+right)//2  mid of row
-        # if target <matrix[mid] and target<matrix[mid-1][0]:
-        #     right=mid
-        # if  target <matrix[mid] and target>matrix[mid-1][0]:
-        #     the neede row is find
-        #     then we will try to find the element based on the colomun by using normal bynary search
-        # if target >matrix[mid] and target<matrix[mid+1][0]:
-        #     left=mid
-        # if target >matrix[mid] and target>matrix[mid-1][0]:
-        #      then we will try to find the element based on the colomun by using normal bynary search
-        left=0
-        right= len(matrix)-1
-        # lc=0
-        # rc=len(matrix[0])-1
+
+        left,right=0,col-1
         while left <= right:
-            mid=(left+right)//2
-            if matrix[mid][0]<=target<=matrix[mid][-1]:
-                lc=0
-                rc=len(matrix[0])-1
-                while lc<=rc:
-                    m=(lc+rc)//2
-                    if matrix[mid][m]==target:
-                        return True
-                    elif matrix[mid][m]<target:
-                        lc=m+1
-                    else:
-                        rc=m-1
-                return False
-            elif target <matrix[mid][0]:
-                right=mid-1
-            else:
-                left=mid +1
+            mid = (left + right)//2
+            if matrix[pos][mid] == target:
+                return True
+            if matrix[pos][mid] < target:
+                left = mid+1
+            elif matrix[pos][mid] > target:
+                right = mid -1
         return False
+
+        
